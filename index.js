4daf2415c5ce6a4a4a6dd2353b7d6557482fc440
Wayne <820853182@qq.com>
2022-11-07 19:06:36 +0800

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+Version: 1.73.0 (Universal)
+Commit: 8fa188b2b301d36553cbc9ce1b0a146ccb93351f
+Date: 2022-11-01T15:38:50.881Z
+Electron: 19.0.17
+Chromium: 102.0.5005.167
+Node.js: 16.14.2
+V8: 10.2.154.15-electron.0
+OS: Darwin x64 22.1.0
+Sandboxing: No
+ */
+
+// Indent cannot be fixed when saving, and ^5 to ^7 do fine.
+  const foo = 'foo'
+  console.log(foo)
